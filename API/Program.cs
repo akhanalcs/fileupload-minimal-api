5388313002456d8ab3d83680e9c019ff9a1ebcac
Ashish Khanal <affableashk@gmail.com>
2023-09-27 15:22:29 -0400

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAntiforgery();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+// Add my file upload endpoint
+app.MapPost("/upload_many", async (IFormFileCollection myFiles) =>
+{
+    foreach (var file in myFiles)
+    {
+        // ...
+    }
+});
+
+app.Run();
